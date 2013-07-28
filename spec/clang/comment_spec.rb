438b5af3f46fd8092c53fc987833528807934162
Carlos Martín Nieto <cmn@dwim.me>
2013-07-28 19:42:30 +0200

    Start testing comments

---
+++ spec/clang/comment_spec.rb
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# Copyright, 2013, by Carlos Martín Nieto <cmn@dwim.me>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require 'spec_helper'
+
+describe Comment do
+	let(:cursor) { Index.new.parse_translation_unit(fixture_path("docs.h")).cursor }
+	let (:comment) { find_first(cursor, :cursor_function).parsed_comment }
+
+	it "can be obtained from a cursor" do
+		comment.should be_kind_of(Comment)
+		comment.kind.should equal(:comment_full)
+	end
+
+	it "can parse the brief description" do
+		para = comment.child
+		para.kind.should equal(:comment_paragraph)
+		text = para.child
+		text.kind.should equal(:comment_text)
+		text.text.strip.should eq("Short explanation")
+	end
+
+	it "can parse the longer description" do
+		para = comment.child(1)
+		para.kind.should equal(:comment_paragraph)
+		para.num_children.should equal(2)
+		text = para.child
+
+		lines = (0..para.num_children-1).map do |i|
+			para.child(i).text
+		end
+
+		lines.should eq([" This is a longer explanation",
+				 " that spans multiple lines"])
+	end
+
+end
