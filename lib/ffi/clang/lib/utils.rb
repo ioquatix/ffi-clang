f4df16bf05dc26075083a0c82d2ea5acb553d28b
Masahiro Sano <sabottenda@gmail.com>
2014-02-02 16:48:07 +0900

    add clang version string apis

--- lib/ffi/clang/lib/utils.rb
+++ lib/ffi/clang/lib/utils.rb
@@ -1,5 +1,4 @@
-# Copyright, 2010-2012 by Jari Bakken.
-# Copyright, 2013, by Samuel G. D. Williams. <http://www.codeotaku.com>
+# Copyright, 2014 by Masahiro Sano.
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -19,36 +18,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require 'ffi'
-require "rbconfig"
+require 'ffi/clang/lib/string'
 
-module FFI::Clang
-	class Error < StandardError
-	end
-
-	def self.platform
-		os = RbConfig::CONFIG["host_os"]
-
-		case os
-		when /darwin/
-			:darwin
-		when /linux/
-			:linux
-		when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-			:windows
-		else
-			os
+module FFI
+	module Clang
+		module Lib
+			attach_function :get_clang_version, :clang_getClangVersion, [], CXString.by_value
 		end
 	end
 end
-
-require 'ffi/clang/utils'
-require 'ffi/clang/lib'
-require 'ffi/clang/index'
-require 'ffi/clang/translation_unit'
-require 'ffi/clang/diagnostic'
-require 'ffi/clang/cursor'
-require 'ffi/clang/source_location'
-require 'ffi/clang/source_range'
-require 'ffi/clang/unsaved_file'
-
