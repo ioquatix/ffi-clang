d6284d4bf1f37fe275df1112a662b0813200b411
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-09-12 16:18:21 +1200

    Remove old code.

--- Gemfile
+++ Gemfile
@@ -2,12 +2,3 @@ source 'https://rubygems.org'
 
 gemspec
 
-group :development do
-	gem 'transpec', git: "https://github.com/yujinakayama/transpec.git"
-	gem 'pry'
-end
-
-group :test do
-	gem 'simplecov'
-	gem 'coveralls', require: false
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 
 require_relative '../lib/ffi/clang'
 
-require 'pry'
-
 include FFI::Clang
 
 TMP_DIR = File.expand_path("tmp", __dir__)
@@ -34,18 +32,4 @@ RSpec.configure do |config|
 	config.example_status_persistence_file_path = ".rspec_status"
 	
 	config.include ClangSpecHelper
-	
-	supported_versions = ['3.4', '3.5', '3.6', '3.7', '3.8', '3.9', '4.0']
-	current_version = ENV['LLVM_VERSION'] || supported_versions.last
-	supported_versions.reverse_each { |version|
-		break if version == current_version
-		sym = ('from_' + version.tr('.', '_')).to_sym
-		config.filter_run_excluding sym => true
-	}
-
-	supported_versions.each { |version|
-		break if version == current_version
-		sym = ('upto_' + version.tr('.', '_')).to_sym
-		config.filter_run_excluding sym => true
-	}
 end
